5cb3667ddd2f257ad69dcf9486e0f4d24d638eba
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-05-29 15:25:17 -0300

    Commited

---
+++ javascript/database.js
@@ -0,0 +1,17 @@
+const {
+    createPool
+} = require('mysql');
+const pool = createPool({
+    host: "spacedwog.mysql.uhserver.com",
+    user: "spacedwog",
+    password: "KIMERAbeta12@",
+    database: "spacedwog",
+    connectionLimit: 10
+});
+
+pool.query('select * from projetos', (err, result, fields)=>{
+    if(err){
+        return console.log(err);
+    }
+    return console.log(result);
+});
