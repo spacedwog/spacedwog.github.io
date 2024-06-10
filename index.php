c63813ed69af2b8b9f756645a303c59b5f7fa25e
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-06-10 17:07:18 -0300

    Commited

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <?php include"database/conexao.php" ?>
     <meta charset="utf-8">
     <link href="./css/menu.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
