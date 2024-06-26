bb8bae47d0f1ffe2aaee7f01ac335cf9993d4fea
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-06-26 18:15:48 -0300

    Commited

---
+++ database/select.php
@@ -0,0 +1,13 @@
+<?php
+    include_once "conexao.php";
+
+    # Seleciona o banco de dados 
+    mysqli_select_db( $database ) or die( 'Erro na seleção do banco' );
+
+    # Executa a query desejada 
+    $query = "SELECT * FROM projetos"; 
+    $result_query = mysqli_query( $query ) or die(' Erro na query:' . $query . ' ' . mysqli_error() ); 
+
+    # Exibe os registros na tela 
+    while ($row = mysqli_fetch_array( $result_query )) { print $row[id_projeto] . " -- " . $row[nome_projeto] . " -- " . $row[tipo_projeto]; }
+?>
