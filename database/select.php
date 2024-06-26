7aa29187aa886aee7137e60b5579d280db808457
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-06-26 18:17:05 -0300

    Commited

--- database/select.php
+++ database/select.php
@@ -5,7 +5,7 @@
     mysqli_select_db( $database ) or die( 'Erro na seleção do banco' );
 
     # Executa a query desejada 
-    $query = "SELECT * FROM projetos"; 
+    $query = "SELECT id_projeto, nome_projeto, tipo_projeto FROM projetos"; 
     $result_query = mysqli_query( $query ) or die(' Erro na query:' . $query . ' ' . mysqli_error() ); 
 
     # Exibe os registros na tela 
