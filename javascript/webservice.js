a5a3a494c4ea335a927dbb015e4edf01f0796acb
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-06-26 17:55:23 -0300

    Commited

--- javascript/webservice.js
+++ javascript/webservice.js
@@ -2,6 +2,6 @@ function getItems() {
     fetch('./database/select.php')
       .then(response => response.json())
       .then(data => _displayItems(data))
-      alert(data)
       .catch(error => console.error('Unable to get items.', error));
+      alert(data);
 }  
