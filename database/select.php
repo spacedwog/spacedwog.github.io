8a73f3be1c5582451ce83a3fb1f4c596291c17bc
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-06-26 18:25:56 -0300

    Commited

--- database/select.php
+++ database/select.php
@@ -9,6 +9,8 @@
     $result_query = mysqli_query( $query ) or die(' Erro na query:' . $query . ' ' . mysqli_error() ); 
 
     # Exibe os registros na tela 
-    while ($row = mysqli_fetch_array( $result_query )) { print $row[id_projeto] . " -- " . $row[nome_projeto] . " -- " . $row[tipo_projeto]; }
-
+    while ($row = mysqli_fetch_array( $result_query )) {
+        print $row[id_projeto] . " -- " . $row[nome_projeto] . " -- " . $row[tipo_projeto]; 
+        $data = JSON.parse($row);
+    }
 ?>
