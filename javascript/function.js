57f6fd6580d6425db8f5bab5d17ca5343e13fa6a
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-07-09 15:02:24 -0300

    Commited

--- javascript/function.js
+++ javascript/function.js
@@ -5,4 +5,26 @@ function updatemenu() {
     }else{
       document.getElementById('menu').style.borderRadius = '10px';
     }
+}
+
+function social_media(social_media){
+  let src;
+  switch(social_media){
+    case 'instagram':
+      src = 'https://www.instagram.com/spacedwog/';
+    break;
+    
+    case 'pinterest':
+      src = 'https://br.pinterest.com/spacedwog/';
+    break;
+
+    case 'twitter':
+      src = 'https://x.com/spacedwog';
+    break;
+
+    case 'linkedin':
+      src = 'https://www.linkedin.com/in/felipersantos1/';
+    break;
   }
+  window.open(src);
+}
