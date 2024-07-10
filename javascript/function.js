89833e54916c1b67cee4227dadef22b10b17738b
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-07-10 18:08:38 -0300

    Commited

--- javascript/function.js
+++ javascript/function.js
@@ -7,6 +7,63 @@ function updatemenu() {
     }
 }
 
+function change_article(article){
+
+  let src;
+
+  switch(article){
+    case 'gourmet_catering':
+      src = 'https://www.w3schools.com/w3images/hamburger.jpg';
+    break;
+
+    case 'food_blog':
+      src = 'https://www.w3schools.com/w3images/sandwich.jpg';
+    break;
+
+    case 'cafe':
+      src = 'https://www.w3schools.com/w3images/coffeehouse.jpg';
+    break;
+
+    case 'clothing_store':
+      src = 'https://www.w3schools.com/w3images/jeans.jpg';
+    break;
+
+    case 'interior_design':
+      src = 'https://www.w3schools.com/w3images/kitchenconcrete.jpg';
+    break;
+    
+    case 'modal_restaurant':
+      src = 'https://www.w3schools.com/w3images/onepage_restaurant.jpg';
+    break;
+    
+    case 'pizza_restaurant':
+      src = 'https://www.w3schools.com/w3images/pizza.jpg';
+    break;
+    
+    case 'startup':
+      src = 'https://www.w3schools.com/w3images/mac.jpg';
+    break;
+    
+    case 'marketing':
+      src = 'https://www.w3schools.com/w3images/coffee.jpg';
+    break;
+    
+    case 'hotel':
+      src = 'https://www.w3schools.com/w3images/hotel.jpg';
+    break;
+    
+    case 'travel_agency':
+      src = 'https://www.w3schools.com/w3images/beach3.jpg';
+    break;
+    
+    case 'house_design':
+      src = 'https://www.w3schools.com/w3images/house1.jpg';
+    break;
+  }
+  const img_article = document.getElementById('article_image');
+  img_article.src = src;
+}
+
 function social_media(social_media){
   let src;
   switch(social_media){
