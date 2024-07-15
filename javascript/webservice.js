b33903af58d5134ad20d5dd082897df502fdb2e7
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-07-15 12:38:28 -0300

    Commited

--- javascript/webservice.js
+++ javascript/webservice.js
@@ -4,3 +4,13 @@ function getItems() {
       .then(data => _displayItems(data))
       .catch(error => console.error('Unable to get items.', error));
 }
+
+function tipo_botao(botao){
+  alert(botao);
+  
+  // Check browser support
+  if(typeof(Storage) !== "undefined"){
+    // Store
+    localStorage.setItem("botao_clicado", botao);
+  }
+}
