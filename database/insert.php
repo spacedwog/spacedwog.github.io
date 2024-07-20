216a054d2c2a7be06043d1acfac135f37a050348
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-07-20 10:54:33 -0300

    Commited

--- database/insert.php
+++ database/insert.php
@@ -6,16 +6,11 @@
     $subject = filter_input(INPUT_POST, 'subject');
     $message = filter_input(INPUT_POST, 'message');
 
-    # Seleciona o banco de dados
-    mysqli_select_db( $conn ) or die( 'Erro na seleção do banco' );
-
     # Executa a query desejada
     $comando = "INSERT INTO mensagem";
     $campos = "(nome, email, subject, message)";
     $values = "VALUES('$name', '$email', '$subject', '$message')";
     $query = $comando. $campos. $values;
-    $
-    $result_query = mysqli_query( $query ) or die(' Erro na query:' . $query . ' ' . mysqli_error() );
 
     if (mysqli_query($conn, $query)) {
       echo "New record created successfully";
