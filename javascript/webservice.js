337d3599cf34ab666c12572acc60b569365a5a3d
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-03 16:54:12 -0300

    Commited

--- javascript/webservice.js
+++ javascript/webservice.js
@@ -1,9 +1,1 @@
-function getProjects() {
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        document.getElementById("txtHint").innerHTML = this.responseText;
-    }
-    xhttp.open("GET", "select.php?q=" + str);
-    xhttp.send();
-}
+function getProjects() {}
