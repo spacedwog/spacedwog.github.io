6a0f48061196a31c68b138519df5df24eed696f9
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-03 21:32:59 +0000

    Commited

--- index1.java
+++ index1.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class index {  
+public class index1 {  
   public static void main(String[] args) {  
     try {
       File myObj = new File("usuario.json");
