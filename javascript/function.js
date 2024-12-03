5974a305333f0f5d74550dae0e5de05949987cd6
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-03 23:49:05 +0000

    Commited

--- javascript/function.js
+++ javascript/function.js
@@ -1,32 +1,3 @@
-//Cookies
-    //Enviar para cookies
-        //Nome Do Usuário
-function setCookie(cname, cvalue){
-    document.cookie = cname + "=" + cvalue + ";";
-}
-    //Receber de Cookie
-function getCookie(cname){
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-
-function checkCookie() {
-    let user = getCookies("username");
-    if (user != "") {
-      alert("Welcome again User: " + user);
-    }
-  }
 //Webservice
     //Atualizar Menu                                
 function updatemenu() {
