dab9a700795583990c9e7cf7091815390dc878bc
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-03 21:15:21 +0000

    Commited

--- index.java
+++ index.java
@@ -1,5 +1,20 @@
-public class index {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class index {  
+  public static void main(String[] args) {  
+    try {
+      File myObj = new File("filename.txt");
+      Scanner myReader = new Scanner(myObj);  
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    } 
   }  
+}
