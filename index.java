11d9488f5bf6294d789c9955d2fee31e4ec55759
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-03 21:17:23 +0000

    Commited

--- index.java
+++ index.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class index {  
   public static void main(String[] args) {  
     try {
-      File myObj = new File("filename.txt");
-      Scanner myReader = new Scanner(myObj);  
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
+      File myObj = new File("usuario.txt");
+        try (Scanner myReader = new Scanner(myObj)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            } }
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
-    } 
+    }
   }  
 }
