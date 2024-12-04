8d02e9831c4de57a834f7e24e2ad849d3ca0dc75
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-04 16:12:08 +0000

    Commited

--- renderizar_foto.java
+++ renderizar_foto.java
@@ -1,15 +1,23 @@
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.Scanner;
 
 public class renderizar_foto {
   public static void main(String[] args) {
     try {
       File myObj = new File("imagem/Spacedwog.png");
+      Scanner myReader = new Scanner(myObj);
       if (myObj.createNewFile()) {
         System.out.println("File created: " + myObj.getName());
       }
       else {
         System.out.println("File already exists.");
+        while (myReader.hasNextLine()) {
+          String data = myReader.nextLine();
+          System.out.println(data);
+        }
+        myReader.close();
+
       }
     } catch (IOException e) {
       System.out.println("An error occurred: " + e.toString());
