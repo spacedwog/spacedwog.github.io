6559454aa1aa58f9d38666c9532f735e1131c9ac
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-06 21:45:42 +0000

    Commited

---
+++ javascript/coockie.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieBar = document.getElementById("cookie-bar");
+    const acceptButton = document.getElementById("accept-cookies");
+
+    // Checa se o cookie já foi aceito
+    if (localStorage.getItem("cookiesAccepted")) {
+        cookieBar.style.display = "none";
+    }
+
+    // Ação ao clicar no botão de aceitar
+    acceptButton.addEventListener("click", function () {
+    localStorage.setItem("cookiesAccepted", "true");
+    cookieBar.style.display = "none";
+    });
+});
