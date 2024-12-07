06cf262dce07b3d7f741610e1a3e07462312056b
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-07 10:27:10 +0000

    Commited

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -5,15 +5,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Checa se o cookie já foi aceito
     if (localStorage.getItem("cookiesAccepted")) {
-        cookieBar.style.display = "none";
+        cookieBar.style.display = "display";
     }
-
     // Ação ao clicar no botão de aceitar
     acceptButton.addEventListener("click", function () {
-    localStorage.setItem("cookiesAccepted", "true");
-    cookieBar.style.display = "none";
-    console.log("Cookie aceito");
-    checkCookie();
-    console.log("Cookie checado");
+        localStorage.setItem("cookiesAccepted", "true");
+        cookieBar.style.display = "none";
     });
 });
+
+function setCookie(cname, cvalue){
+    document.cookie = cname + "=" + cvalue + ";";
+}
+//Receber de Cookie
+function getCookie(cname){
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+        
+function checkCookie() {
+    let user = getCookies("username");
+    if (user != "") {
+        alert("Welcome again User: " + user);
+    }
+}
