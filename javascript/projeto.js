2569551c5ce5bbe428c7af4fc80927d0a8114eaf
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-12 17:49:58 +0000

    Commited

--- javascript/projeto.js
+++ javascript/projeto.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-    $("a").click(function(){
-        alert("Link clicado!" + $(this).attr("href"));
-      //$(this).hide();
-    });
-  });
+function createForm(form_assunto) {
+    const about = document.getElementById('about').value;
+    const team = document.getElementById('team').value;
+    const work = document.getElementById('work').value;
+    const courses = document.getElementById('courses').value;
+    const pricing = document.getElementById('pricing').value;
+    const contact = document.getElementById('contact').value;
+
+    alert(about);
+}
